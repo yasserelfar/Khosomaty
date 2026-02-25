477a56ec1849753571f0dbedbe3729dcf1105db5
yassoelfar <yassermuhamed78@gmail.com>
2026-02-25 17:26:55 +0200

    change repo again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "Khosomaty" : "/",
+  base: process.env.NODE_ENV === "production" ? "/Khosomaty/" : "/",
   plugins: [
     tailwindcss(),
     react({
